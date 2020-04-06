d6db620dcd59a5ef90fea349a2c9d4616397351b
ZYY12138dhc <45678400+ZYY12138dhc@users.noreply.github.com>
2020-04-06 13:14:19 +0800

    day_01

---
+++ day_01.py
@@ -0,0 +1,31 @@
+print("hello world!")
+
+
+
+"""
+知识点
+1. python2和python3之前有些语句是不通用的。可在shell里面获得一些帮助
+2.  查询函数帮助：
+    
+    help(print)
+    Help on built-in function print in module builtins:
+
+    print(...)
+    print(value, ..., sep=' ', end='\n', file=sys.stdout, flush=False)
+    
+    Prints the values to a stream, or to sys.stdout by default.
+    Optional keyword arguments:
+    file:  a file-like object (stream); defaults to the current sys.stdout.
+    sep:   string inserted between values, default a space.
+    end:   string appended after the last value, default a newline.
+    flush: whether to forcibly flush the stream.
+
+3.  英文输入法,包括标点符号和英文字母
+4   Ctrl+N,新建文件[New File]
+4.  print(" ")print(' ')[打印函数,单引号和双引号都可以]
+    按F5运行程序，出现*号，提示你要保存
+5.  >>> [表示提示符，程序已经准备好]
+6.  程序可以在新建文件中运行，也可以在shell里面运行，按回车键
+
+    
+"""
